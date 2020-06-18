a3239bf628904a39891219591c7417c816dac5fd
rodakd <dawidrodak@yahoo.com>
2020-06-18 13:42:04 +0200

    Add basic welcome screen

--- screens/WelcomeScreen.tsx
+++ screens/WelcomeScreen.tsx
@@ -2,12 +2,24 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { StyleSheet } from 'react-native';
 import { WelcomeProps } from '../navigation/Types';
+import Swiper from 'react-native-swiper';
 
 const WelcomeScreen: React.FC<WelcomeProps> = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
-    </View>
+    <Swiper loop={false} activeDotColor="#12AB34" dotColor="#000">
+      <View style={styles.container}>
+        <Text> What's up </Text>
+      </View>
+      <View style={styles.container}>
+        <Text> How you doin </Text>
+      </View>
+      <View style={styles.container}>
+        <Text> I'm fine </Text>
+      </View>
+      <View style={styles.container}>
+        <Text> And you? </Text>
+      </View>
+    </Swiper>
   );
 };
 
